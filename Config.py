6aca43e236f11e869db3a65382bd332b4a2aa11e
smosameer155-star <smosameer155@gmail.com>
2026-08-05 19:23:08 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,24 +1,28 @@
-
 import os
 
 # Telegram API
-API_ID = int(os.getenv("API_ID", "123456"))
-API_HASH = os.getenv("API_HASH", "YOUR_API_HASH")
+API_ID = int(os.getenv("API_ID", "0"))
+API_HASH = os.getenv("API_HASH", "")
 
 # Bot Token
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
 # Owner ID
-OWNER_ID = int(os.getenv("OWNER_ID", "123456789"))
+OWNER_ID = int(os.getenv("OWNER_ID", "0"))
 
 # MongoDB
 MONGO_DB_URI = os.getenv("MONGO_DB_URI", "")
 
-# Log Group
-LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", "-1001234567890"))
-
-# Session String
+# String Session
 STRING_SESSION = os.getenv("STRING_SESSION", "")
 
-# Bot Name
-BOT_NAME = "Iqra Music & Video Bot"
+# Log Group
+LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", "0"))
+
+# Bot Information
+BOT_NAME = "Iqra Music Bot"
+BOT_USERNAME = "YourBotUsername"
+
+# Support
+SUPPORT_CHAT = "YourSupportGroup"
+UPDATES_CHANNEL = "YourUpdatesChannel"
